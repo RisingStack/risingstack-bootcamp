f4c0b7c507c534d83e0627bcc15040a0bcd07779
Andras Toth <andras.toth@risingstack.com>
2017-07-06 10:29:28 +0200

    Step 2: solution

--- models/github/api.js
+++ models/github/api.js
@@ -2,9 +2,34 @@
 
 const request = require('request-promise-native')
 
-function searchRepositories(query = {}) {}
+const API_URL = 'https://api.github.com'
+const USER_AGENT = 'RisingStack-Bootcamp'
 
-function getContributors(repository, query = {}) {}
+function searchRepositories(query = {}) {
+  return request({
+    method: 'GET',
+    uri: `${API_URL}/search/repositories`,
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      'User-Agent': USER_AGENT
+    },
+    qs: query,
+    json: true
+  })
+}
+
+function getContributors(repository, query = {}) {
+  return request({
+    method: 'GET',
+    uri: `${API_URL}/repos/${repository}/stats/contributors`,
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      'User-Agent': USER_AGENT
+    },
+    qs: query,
+    json: true
+  })
+}
 
 module.exports = {
   searchRepositories,
--- models/github/api.spec.js
+++ models/github/api.spec.js
@@ -5,6 +5,35 @@ const { expect } = require('chai')
 const api = require('./api')
 
 describe('GitHub API', () => {
-  it('should search repositories')
-  it('should get contributors')
+  it('should search repositories', async () => {
+    const githubAPI = nock('https://api.github.com', {
+      reqheaders: {
+        accept: 'application/vnd.github.v3+json',
+        'user-agent': 'RisingStack-Bootcamp'
+      }
+    })
+      .get('/search/repositories')
+      .query({ q: 'language:javascript' })
+      .reply(200, { items: [] })
+
+    const result = await api.searchRepositories({ q: 'language:javascript' })
+    expect(githubAPI.isDone()).to.eql(true)
+    expect(result).to.eql({ items: [] })
+  })
+
+  it('should get contributors', async () => {
+    const repository = 'owner/repository'
+    const githubAPI = nock('https://api.github.com', {
+      reqheaders: {
+        accept: 'application/vnd.github.v3+json',
+        'user-agent': 'RisingStack-Bootcamp'
+      }
+    })
+      .get(`/repos/${repository}/stats/contributors`)
+      .reply(200, [{ author: {}, weeks: [] }])
+
+    const result = await api.getContributors(repository)
+    expect(githubAPI.isDone()).to.eql(true)
+    expect(result).to.eql([{ author: {}, weeks: [] }])
+  })
 })
